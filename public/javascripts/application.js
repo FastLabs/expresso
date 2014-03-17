be451ce6e647226d1b1a865b0ecc15f4141606d8
FastLabs <bulavitchi@gmail.com>
2014-03-17 07:01:27 +0000

    working on fix a

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,39 +9,7 @@ angular.module("application", ["rules"])
                 , {title: "Commodities", tag: "com"}
         ]);
 
-angular.module("rules", [])
-    .value("ruleApps", {
-       "ir": {name: "Interest Rates Eligibility"
-           , ruleSets: [
-               {name: "Rule Set 1", tags: [], rules: [
-                   {name:"Event Type"}, {name: "Creational"}] }
-               , {name : "Rule Set 2", tags: [], rules: []}
-           ]
-       },
-        "eq": {name: "Equities Eligibility"
-            , ruleSets: [
-                {name: "Rule Set 1", tags: [], rules: [] }
-                , {name : "Rule Set 2", tags: [], rules: []}
-            ]
-        },
-        "fx": {name: "FX Eligibility"
-            , ruleSets: [
-                {name: "Rule Set 1", tags: [], rules: [] }
-                , {name : "Rule Set 2", tags: [], rules: []}
-            ]
-        }
-    })
 
-    .factory("ruleService", function(ruleApps) {
-
-        var ruleSets = function (ruleApp) {
-            return ruleApps[ruleApp];
-        }
-
-        return {
-            getRuleSets : ruleSets
-        }
-    });
 
 function appMenuController($scope, appmenu, ruleService) {
 
